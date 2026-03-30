29118d02d199d056079d23eab3980cd4b61e41b2
Aguerrerop19 <guerreropabrahamj@gmail.com>
2026-03-30 14:13:49 -0400

    Switch to Base Sepolia network

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,7 +2,7 @@ require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
 
 const PRIVATE_KEY = process.env.PRIVATE_KEY || "0x0000000000000000000000000000000000000000000000000000000000000001";
-const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL || "";
+const BASE_SEPOLIA_RPC_URL = process.env.BASE_SEPOLIA_RPC_URL || "";
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "";
 
 /** @type import('hardhat/config').HardhatUserConfig */
@@ -17,13 +17,25 @@ module.exports = {
     },
   },
   networks: {
-    sepolia: {
-      url: SEPOLIA_RPC_URL,
+    baseSepolia: {
+      url: BASE_SEPOLIA_RPC_URL,
       accounts: [PRIVATE_KEY],
-      chainId: 11155111,
+      chainId: 84532,
     },
   },
   etherscan: {
-    apiKey: ETHERSCAN_API_KEY,
+    apiKey: {
+      baseSepolia: ETHERSCAN_API_KEY,
+    },
+    customChains: [
+      {
+        network: "baseSepolia",
+        chainId: 84532,
+        urls: {
+          apiURL: "https://api-sepolia.basescan.org/api",
+          browserURL: "https://sepolia.basescan.org",
+        },
+      },
+    ],
   },
 };
